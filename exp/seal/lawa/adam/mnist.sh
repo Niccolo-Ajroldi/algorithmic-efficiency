9d41021ad05f6e392d178ded8064d06679170cc6
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2024-03-18 15:26:21 +0100

    lawa k=1 tested on CPUs and GPUs

---
+++ exp/seal/lawa/adam/mnist.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source ~/.bashrc
+conda activate alpe
+
+export CUDA_VISIBLE_DEVICES=''
+
+# Job specific vars
+workload=mnist
+dataset=MNIST
+submission=submissions/lawa/mnist/adam.py
+search_space=exp/seal/lawa/space_1.json
+trials=1
+name="rng_adam_01_cpu"
+
+# Print GPU infos
+# nvidia-smi
+
+# Execute python script
+python3 $CODE_DIR/submission_runner.py \
+    --workload=$workload \
+    --framework=pytorch \
+    --tuning_ruleset=external \
+    --data_dir=$DATA_DIR/$dataset \
+    --submission_path=$submission \
+    --tuning_search_space=$search_space \
+    --num_tuning_trials=$trials \
+    --experiment_dir=$EXP_DIR  \
+    --experiment_name=$name \
+    --overwrite \
+    --use_wandb \
+    --save_checkpoints=False \
+    --max_global_steps 1000 \
+    --rng_seed=1996
