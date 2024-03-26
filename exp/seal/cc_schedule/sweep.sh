7c55a5b6939f4c566c2d6bf7c06f13f0afe0b592
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2024-03-26 14:49:52 +0100

    cc_schedule tests

--- exp/seal/cc_schedule/sweep.sh
+++ exp/seal/cc_schedule/sweep.sh
@@ -7,11 +7,12 @@ conda activate alpe
 workload=ogbg
 dataset=ogbg
 submission=submissions/cc_schedule/nadamw.py
-search_space=exp/seal/cc_schedule/trial_5.json
-trials=1
-name="cc_schedule_debug_01"
+search_space=exp/seal/cc_schedule/sweep.json
+trials=3
+name="cc_schedule_sweep"
 
-trial_index=0
+trial_index=$1
+
 export CUDA_VISIBLE_DEVICES=$trial_index
 python3 \
     $CODE_DIR/submission_runner.py \
@@ -26,9 +27,9 @@ python3 \
     --trial_index=$trial_index \
     --experiment_dir=$EXP_DIR  \
     --experiment_name=$name \
-    --overwrite \
+    --resume_last_run \
     --save_checkpoints=False \
+    --save_intermediate_checkpoints=False \
     --use_wandb \
     --extra_wandb_logging \
     --rng_seed=1996
-
