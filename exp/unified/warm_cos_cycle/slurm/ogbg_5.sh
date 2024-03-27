bb764791a8a0bddf2242646cf6aa098aac7fa33d
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2024-03-27 15:51:36 +0100

    added nadamw_warm_cos

---
+++ exp/unified/warm_cos_cycle/slurm/ogbg_5.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#SBATCH --job-name=resnet_sweep
+#SBATCH --array=1-36
+#SBATCH --error=/ptmp/najroldi/logs/algoperf/err/%x_%A_%a.err
+#SBATCH --output=/ptmp/najroldi/logs/algoperf/out/%x_%A_%a.out
+#SBATCH --time=24:00:00
+#SBATCH --ntasks 1
+#SBATCH --requeue
+#SBATCH --gres=gpu:a100:4
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=500000
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate alpe
+
+# Env vars
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+user=najroldi
+export HOME="/home/${user}"
+export CODE_DIR="/home/${user}/algorithmic-efficiency"
+export EXP_DIR="/fast/${user}/exp/algoperf"
+export DATA_DIR="/fast/najroldi/data"
+
+dataset=ogbg
+workload=ogbg
+submission=submissions/warm_cos_cycle/nadamw_warm_cos_cycle.py
+search_space=exp/unified/warm_cos_cycle/json/trial_5_2cycle.json
+exp_name=resnet_sweep_check_1
+study=1
+num_tuning_trials=${SLURM_ARRAY_TASK_MAX}
+trial_index=${SLURM_ARRAY_TASK_ID}
+rng_seed=1
+
+chmod +x exp/unified/warm_cos_cycle/slurm/auto_run.sh
+
+srun exp/unified/warm_cos_cycle/slurm/auto_run.sh \
+  ${dataset} \
+  ${workload} \
+  ${submission} \
+  ${search_space} \
+  ${exp_name} \
+  ${study} \
+  ${num_tuning_trials} \
+  ${trial_index} \
+  ${rng_seed}
