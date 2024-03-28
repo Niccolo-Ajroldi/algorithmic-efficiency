73dfb1fa3f04d15418281bba9e26cc6f6635c817
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2024-03-29 00:38:05 +0100

    mc

---
+++ exp/unified/lawa_trick/slurm/ogbg.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#SBATCH --job-name=lawa_trick
+#SBATCH --array=1-15
+#SBATCH --error=/ptmp/najroldi/logs/algoperf/err/%x_%A_%a.err
+#SBATCH --output=/ptmp/najroldi/logs/algoperf/out/%x_%A_%a.out
+#SBATCH --time=06:00:00
+#SBATCH --ntasks 1
+#SBATCH --requeue
+# --- 4 GPUs on a full node ---
+#SBATCH --gres=gpu:a100:4
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=500000
+
+source ~/.bashrc
+conda activate alpe
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export CODE_DIR=~/algorithmic-efficiency
+export EXP_DIR=/ptmp/najroldi/exp/algoperf
+export DATA_DIR=/ptmp/najroldi/data
+
+# Workload
+dataset=ogbg
+workload=ogbg
+
+# Same seed across trials
+study=1
+rng_seed=1
+
+# Submission
+submission=submissions/lawa_trick/lawa.py
+search_space=exp/unified/lawa_trick/json/trial_5.json
+
+# Experiment name
+base_name="lawa_trick_01"
+
+# Set config
+experiment_name="${base_name}"
+num_tuning_trials=${SLURM_ARRAY_TASK_MAX}
+trial_index=${SLURM_ARRAY_TASK_ID}
+
+# Librispeech tokenizer path
+tokenizer_path=''
+if [ "$dataset" = "librispeech" ]; then
+    tokenizer_path="${DATA_DIR}/librispeech/spm_model.vocab"
+fi
+
+# Data dir
+data_dir_2=$DATA_DIR/$dataset
+
+# Execute python script
+torchrun \
+  --redirects 1:0,2:0,3:0 \
+  --standalone \
+  --nnodes=1 \
+  --nproc_per_node=4 \
+  $CODE_DIR/submission_runner.py \
+  --workload=$workload \
+  --framework=pytorch \
+  --tuning_ruleset=external \
+  --data_dir=$data_dir_2 \
+  --imagenet_v2_data_dir=$DATA_DIR/$dataset \
+  --librispeech_tokenizer_vocab_path=$tokenizer_path \
+  --submission_path=$submission \
+  --tuning_search_space=$search_space \
+  --num_tuning_trials=$num_tuning_trials \
+  --trial_index=$trial_index \
+  --experiment_dir=$EXP_DIR  \
+  --experiment_name=$experiment_name \
+  --save_intermediate_checkpoints=False \
+  --save_checkpoints=False \
+  --resume_last_run \
+  --use_wandb \
+  --rng_seed=$rng_seed \
+  --fixed_space
