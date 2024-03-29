796b4c76e4c946d1cecd7060dc1ad15d82776aa8
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2024-03-29 12:04:58 +0100

    vit EVAL_SEC ripristinato

--- algorithmic_efficiency/workloads/imagenet_vit/workload.py
+++ algorithmic_efficiency/workloads/imagenet_vit/workload.py
@@ -85,9 +85,8 @@ class BaseImagenetVitWorkload(BaseImagenetResNetWorkload):
 
   @property
   def eval_period_time_sec(self) -> int:
-    return 12 * 60  # 7 mins. 
-    # return 7 * 60  # 7 mins. # (nico) TODO: REMOVE!!
-
+    return 7 * 60  # 7 mins.
+    
   def _build_dataset(
       self,
       data_rng: spec.RandomState,
