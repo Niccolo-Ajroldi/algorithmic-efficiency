2f9d5519bacbd3043f2f3f83baf4e4ce10860e8c
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2024-04-03 15:30:05 +0200

    lawa ema

---
+++ exp/unified/lawa_ema_inner/slurm/auto_run.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+CODE_DIR=~/algorithmic-efficiency
+EXP_DIR=/ptmp/najroldi/exp/algoperf
+DATA_DIR=/ptmp/najroldi/data
+
+# Job specific vars
+dataset=$1
+workload=$2
+submission=$3
+search_space=$4
+name=$5
+study=$6
+num_tuning_trials=$7
+
+# $8 should be $(SLURM_ARRAY_TASK_ID)
+trial_index=$8
+
+# Same seed across trials
+rng_seed=$9
+
+# Experiment name
+experiment_name="${name}"
+
+# Librispeech tokenizer path
+tokenizer_path=''
+if [ "$dataset" = "librispeech" ]; then
+    tokenizer_path="${DATA_DIR}/librispeech/spm_model.vocab"
+fi
+
+# Imagenet is in a different folder on mpi cluster
+data_dir_2=$DATA_DIR/$dataset
+# if [ "$dataset" = "imagenet" ]; then
+#     data_dir="/is/cluster/fast/jpiles/imagenet"
+# fi
+
+# Execute python script
+torchrun \
+  --redirects 1:0,2:0,3:0 \
+  --standalone \
+  --nnodes=1 \
+  --nproc_per_node=4 \
+  $CODE_DIR/submission_runner.py \
+  --workload=$workload \
+  --framework=pytorch \
+  --tuning_ruleset=external \
+  --data_dir=$data_dir_2 \
+  --imagenet_v2_data_dir=$DATA_DIR/$dataset \
+  --librispeech_tokenizer_vocab_path=$tokenizer_path \
+  --submission_path=$submission \
+  --tuning_search_space=$search_space \
+  --num_tuning_trials=$num_tuning_trials \
+  --experiment_dir=$EXP_DIR  \
+  --experiment_name=$experiment_name \
+  --save_intermediate_checkpoints=False \
+  --save_checkpoints=False \
+  --resume_last_run \
+  --rng_seed=$rng_seed \
+  --use_wandb \
+  --fixed_space \
+  --trial_index=$trial_index
---
+++ exp/unified/lawa_ema_inner/slurm/srun.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH --job-name=sub_lawaema_01
+#SBATCH --array=1-54
+#SBATCH --error=/ptmp/najroldi/logs/algoperf/err/%x_%A_%a.err
+#SBATCH --output=/ptmp/najroldi/logs/algoperf/out/%x_%A_%a.out
+#SBATCH --time=24:00:00
+#SBATCH --ntasks 1
+#SBATCH --requeue
+#SBATCH --gres=gpu:a100:4
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=500000
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate alpe
+
+# Env vars
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export CODE_DIR=~/algorithmic-efficiency
+export EXP_DIR=/ptmp/najroldi/exp/algoperf
+export DATA_DIR=/ptmp/najroldi/data
+
+dataset=ogbg
+workload=ogbg
+submission=submissions/lawa_ema_trick/lawa.py
+search_space=exp/unified/lawa_ema_inner/json_02/trial_5.json
+exp_name=sub_lawaema_01
+study=1
+num_tuning_trials=${SLURM_ARRAY_TASK_MAX}
+trial_index=${SLURM_ARRAY_TASK_ID}
+rng_seed=1
+
+chmod +x exp/unified/lawa_ema_inner/slurm/auto_run.sh
+
+srun exp/unified/lawa_ema_inner/slurm/auto_run.sh \
+  ${dataset} \
+  ${workload} \
+  ${submission} \
+  ${search_space} \
+  ${exp_name} \
+  ${study} \
+  ${num_tuning_trials} \
+  ${trial_index} \
+  ${rng_seed}
