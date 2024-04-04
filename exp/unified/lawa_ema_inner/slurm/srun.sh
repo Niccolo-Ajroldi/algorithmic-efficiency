1793ee4be9b0f8dcfe8fdb4e88014793cfe4beb3
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2024-04-04 19:37:15 +0200

    mc

--- exp/unified/lawa_ema_inner/slurm/srun.sh
+++ exp/unified/lawa_ema_inner/slurm/srun.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=fastmri_ema_gamble
-#SBATCH --array=1-24
+#SBATCH --job-name=ogbg_dc
+#SBATCH --array=1-6
 #SBATCH --error=/ptmp/najroldi/logs/algoperf/err/%x_%A_%a.err
 #SBATCH --output=/ptmp/najroldi/logs/algoperf/out/%x_%A_%a.out
 #SBATCH --time=03:00:00
 #SBATCH --ntasks 1
 #SBATCH --requeue
 #SBATCH --gres=gpu:a100:4
-#SBATCH --cpus-per-task=24
+#SBATCH --cpus-per-task=48
 #SBATCH --mem=500000
 
 source ~/miniconda3/etc/profile.d/conda.sh
@@ -20,11 +20,11 @@ export CODE_DIR=~/algorithmic-efficiency
 export EXP_DIR=/ptmp/najroldi/exp/algoperf
 export DATA_DIR=/ptmp/najroldi/data
 
-dataset=fastmri
-workload=fastmri
-submission=submissions/lawa_ema_trick_gamble/lawa.py
-search_space=exp/unified/lawa_ema_inner/json_gamble/trial_5.json
-exp_name=sub_lawaema_gamble_05
+dataset=ogbg
+workload=ogbg
+submission=submissions/lawa_ema_trick/lawa.py
+search_space=exp/unified/lawa_ema_inner/json_dc/trial_5.json
+exp_name=sub_lawaema_gamble_09
 study=1
 num_tuning_trials=${SLURM_ARRAY_TASK_MAX}
 trial_index=${SLURM_ARRAY_TASK_ID}
