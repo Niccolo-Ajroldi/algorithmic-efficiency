60c8c1e44d2597f275c8a78d12a0d9e473e1a75d
Niccolo-Ajroldi <n.ajroldi@gmail.com>
2025-01-25 23:00:10 +0100

    adding slurm script

---
+++ script/lawa/slurm.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+
+#SBATCH --job-name=lawa_ema
+#SBATCH --array=1-12
+#SBATCH --error=/ptmp/najroldi/logs/algoperf/err/%x_%A_%a.err
+#SBATCH --output=/ptmp/najroldi/logs/algoperf/out/%x_%A_%a.out
+#SBATCH --time=06:00:00
+#SBATCH --ntasks 1
+#SBATCH --requeue
+# --- 4 GPUs on a full node ---
+#SBATCH --gres=gpu:a100:4
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=500000
+
+## TODO: set wandb dir as well!!
+
+source ~/.bashrc
+conda activate alpe
+
+export CODE_DIR=~/algorithmic-efficiency
+export EXP_DIR=/ptmp/najroldi/exp/algoperf
+export DATA_DIR=/ptmp/najroldi/data
+
+export HTTP_PROXY=$http_proxy
+export HTTPS_PROXY=$https_proxy
+
+# # Will this allow to set pytorch_eval_num_workers=0? -> yes
+# # will it allow to have correct evals on workers>1 ??
+# export OMP_NUM_THREADS=1 
+# export MKL_NUM_THREADS=1
+
+# Job specific vars
+workload=$1
+framework=$2
+submission=$3
+search_space=$4
+num_tuning_trials=$5
+study=$6
+name=$7
+rng_seed=$8
+allow_tf_32=$9
+run_until_the_end=${10}
+target_setting=${11}
+
+cluster_id=${12}
+process_id=${13}
+
+# CONDOR job arrays range from 0 to n-1, so we add +1 here
+# $((...)) is for arithmetic substitution in .sh
+trial_index=$((${process_id} + 1))
+
+workload_list=(
+  criteo1tb
+  fastmri
+  imagenet_resnet
+  imagenet_vit
+  librispeech_conformer
+  librispeech_deepspeech
+  ogbg
+  wmt
+)
+
+# in bash need to explicitly declare an associative array
+declare -A workload_to_dataset=(
+  [criteo1tb]="criteo1tb"
+  [fastmri]="fastmri"
+  [imagenet_resnet]="imagenet"
+  [imagenet_vit]="imagenet"
+  [librispeech_conformer]="librispeech"
+  [librispeech_deepspeech]="librispeech"
+  [ogbg]="ogbg"
+  [wmt]="wmt"
+)
+
+# Determine dataset based on workload
+dataset=${workload_to_dataset[$workload]}
+
+# Experiment name
+experiment_name="${name}_study_${study}"
+
+# Librispeech tokenizer path
+tokenizer_path=''
+if [ "$dataset" = "librispeech" ]; then
+    tokenizer_path="${DATA_DIR}/librispeech/spm_model.vocab"
+fi
+
+# Increase num_workers on imagenet
+pytorch_eval_num_workers=0
+if [ "$dataset" == "imagenet" ] && [ "$framework" == "pytorch" ]; then
+  pytorch_eval_num_workers=2
+fi
+
+# allow_tf_32
+allow_tf_32_flag=False
+if [ "$allow_tf_32" == "1" ]; then
+  allow_tf_32_flag=True
+fi
+
+# run_until_the_end
+run_until_the_end_flag=False
+if [ "$run_until_the_end" == "1" ]; then
+  run_until_the_end_flag=True
+fi
+
+# max_pct_of_global_steps
+max_pct_of_global_steps=1.0
+if [ "$target_setting" == "1" ]; then
+  max_pct_of_global_steps=0.75
+fi
+
+# Execute python script
+OMP_NUM_THREADS=1 torchrun \
+  --redirects 1:0,2:0,3:0 \
+  --standalone \
+  --nnodes=1 \
+  --nproc_per_node=4 \
+  $CODE_DIR/submission_runner.py \
+  --workload=$workload \
+  --framework=$framework \
+  --tuning_ruleset=external \
+  --data_dir=$DATA_DIR/$dataset \
+  --imagenet_v2_data_dir=$DATA_DIR/$dataset \
+  --librispeech_tokenizer_vocab_path=$tokenizer_path \
+  --submission_path=$submission \
+  --tuning_search_space=$search_space \
+  --num_tuning_trials=$num_tuning_trials \
+  --fixed_space \
+  --trial_index=$trial_index \
+  --experiment_dir=$EXP_DIR  \
+  --experiment_name=$experiment_name \
+  --save_intermediate_checkpoints=False \
+  --save_checkpoints=False \
+  --use_wandb \
+  --rng_seed=$rng_seed \
+  --torch_compile=True \
+  --allow_tf32=$allow_tf_32_flag \
+  --run_until_the_end=$run_until_the_end_flag \
+  --halve_CUDA_mem=False \
+  --pytorch_eval_num_workers=$pytorch_eval_num_workers \
+  --resume_last_run \
+  --max_pct_of_global_steps=$max_pct_of_global_steps \
+  --cluster_id $cluster_id \
+  --process_id $process_id
+
+# resuming is needed with multiple paralell processes accessing the same dir
